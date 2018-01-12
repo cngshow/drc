c14b39bc2d4e9028426c56e9fb7ca09bcfd1ccf9
cshupp <cshupp@gmail.com>
2018-01-12 10:54:32 -0500

    React working in trinidad now.

--- config/initializers/webpacker.rb
+++ config/initializers/webpacker.rb
@@ -1,6 +1,6 @@
 class Webpacker::Configuration
   def public_output_path
-    if CATALINA_HOME
+    if (CATALINA_HOME && !Rails.env.development?)
       Pathname.new "#{Rails.root.to_s}/../#{fetch(:public_output_path)}"
     else
       public_path.join(fetch(:public_output_path))
