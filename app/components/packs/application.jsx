61b7cef776e6efbfc2224a3e0c662916bd4e431c
gpbowman-git <gpbowman.git@gmail.com>
2018-01-16 12:30:01 -0500

    initial checkin of material-ui test code

---
+++ app/components/packs/application.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SimpleDialogDemo from './dialog/dialog'
+
+export default class Application extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <SimpleDialogDemo />
+        )
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    ReactDOM.render(
+        <Application />, document.getElementById('app')
+    )
+});
