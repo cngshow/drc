4827745390d859110a5898088b0cd0e44fe4899c
cshupp <cshupp@gmail.com>
2018-02-01 17:11:20 -0500

    websockets working

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,6 @@ require_relative 'application'
 # Initialize the Rails application.
 WINDOWS = (java.lang.System.getProperties['os.name'] =~ /win/i)
 require './lib/logging/logging'
-CATALINA_HOME = java.lang.System.properties['catalina.home']
 Rails.application.initialize!
 original_logger = Rails.logger
 $log_rails.define_singleton_method(:method_missing) do |symbol, args|
